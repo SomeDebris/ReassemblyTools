5bbd656be849b493a6e776b620ec2a6726f81a08
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-05-17 00:54:02 -0700

    loadshapefile: begin to assemble dicts of ships

--- src/ReassemblyTools.jl
+++ src/ReassemblyTools.jl
@@ -26,11 +26,13 @@ function loadshapefile(filename::String)
     shape_dict = Dict{String, Any}()
 
     for idx_shape in eachindex(shapes)
+        new_shape = Array{Dict, 1}(undef, lastindex(shapes))
+
         for scale in shapes[idx_shape][2]
             formatted_scale = Dict{String, AbstractVecOrMat}()
             # Creates the array of vertices. as long as the number of vertices 
             # and 2 wide
-            verts = Array{Float64,2}(undef, length(scale["verts"]), 2)
+            verts = Array{Float64,2}(undef, lastindex(scale["verts"]), 2)
 
             for idx_vtx in eachindex(scale["verts"])
                 verts[idx_vtx, :] = scale["verts"][idx_vtx]
@@ -38,10 +40,13 @@ function loadshapefile(filename::String)
 
             formatted_scale["verts"] = verts
 
-            display(formatted_scale)
+            new_shape[idx_shape] = formatted_scale
+
         end
 
-        shape_dict[shape[1]] = shape[2]
+        display(new_shape)
+
+        shape_dict[shapes[idx_shape][1]] = shapes[idx_shape][2]
     end
 end
 
