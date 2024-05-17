2af8920e450577cdfc78f520428935baee98efb5
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-05-17 01:00:33 -0700

    loadshapefile: add the area of the block's scale to each scale

--- src/ReassemblyTools.jl
+++ src/ReassemblyTools.jl
@@ -26,9 +26,11 @@ function loadshapefile(filename::String)
     shape_dict = Dict{String, Any}()
 
     for idx_shape in eachindex(shapes)
-        new_shape = Array{Dict, 1}(undef, lastindex(shapes))
+        new_shape = Array{Dict, 1}(undef, lastindex(shapes[idx_shape][2]))
 
-        for scale in shapes[idx_shape][2]
+        for idx_scale in eachindex(shapes[idx_shape][2])
+            scale = shapes[idx_shape][2][idx_scale]
+
             formatted_scale = Dict{String, AbstractVecOrMat}()
             # Creates the array of vertices. as long as the number of vertices 
             # and 2 wide
@@ -39,15 +41,17 @@ function loadshapefile(filename::String)
             end
 
             formatted_scale["verts"] = verts
+            formatted_scale["area"] = getpolygonarea(verts[:,1], verts[:,2])
 
-            new_shape[idx_shape] = formatted_scale
-
+            new_shape[idx_scale] = formatted_scale
         end
 
         display(new_shape)
 
-        shape_dict[shapes[idx_shape][1]] = shapes[idx_shape][2]
+        shape_dict[shapes[idx_shape][1]] = new_shape
     end
+
+    shape_dict
 end
 
 
