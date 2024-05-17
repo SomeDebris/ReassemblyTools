d085c731db7786baeb65e6d79b1ef275a3e04124
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-05-17 00:34:42 -0700

    Initial Commit

---
+++ src/ReassemblyTools.jl
@@ -0,0 +1,34 @@
+module ReassemblyTools
+using JSON
+using LinearAlgebra
+
+greet() = print("Hello World!")
+
+struct ShipInfo
+    mass
+    centroid
+    I
+end
+
+function loadshapefile(filename::String)
+    shapes = JSON.parsefile(filename)
+
+    shape_dict = Dict{String, Any}()
+
+    for shape in shapes
+        for scale in shape[2]
+            # Creates the array of vertices. as long as the number of vertices 
+            # and 2 wide
+            verts = Array{Float64,2}(undef, length(scale["verts"]), 2)
+
+            for idx_vtx in 1:length(scale["verts"])
+                verts[idx_vtx, :] = scale["verts"][idx_vtx]
+            end
+        end
+
+        shape_dict[shape[1]] = shape[2]
+    end
+end
+
+
+end # module ReassemblyTools
