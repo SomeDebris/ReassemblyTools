8dc5ce25be0b86331b1262ac5ded84862690f60a
Magnus Ramsey <38709696+SomeDebris@users.noreply.github.com>
2024-05-17 00:49:51 -0700

    loadshapefile: loop over index instead of each actual item

--- src/ReassemblyTools.jl
+++ src/ReassemblyTools.jl
@@ -10,22 +10,35 @@ struct ShipInfo
     I
 end
 
+function getpolygoncentroid(X, Y)
+    X
+end
+
+function getpolygonarea(X, Y)
+    area_sum_items = (X .* circshift(Y, 1)) - (Y .* circshift(X, 1))
+
+    sum(area_sum_items) / 2
+end
+
 function loadshapefile(filename::String)
     shapes = JSON.parsefile(filename)
 
     shape_dict = Dict{String, Any}()
 
-    for shape in shapes
-        for scale in shape[2]
+    for idx_shape in eachindex(shapes)
+        for scale in shapes[idx_shape][2]
+            formatted_scale = Dict{String, AbstractVecOrMat}()
             # Creates the array of vertices. as long as the number of vertices 
             # and 2 wide
             verts = Array{Float64,2}(undef, length(scale["verts"]), 2)
 
-            for idx_vtx in 1:length(scale["verts"])
+            for idx_vtx in eachindex(scale["verts"])
                 verts[idx_vtx, :] = scale["verts"][idx_vtx]
             end
 
-            display(verts)
+            formatted_scale["verts"] = verts
+
+            display(formatted_scale)
         end
 
         shape_dict[shape[1]] = shape[2]
